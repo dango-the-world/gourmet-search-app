d99445a249a8b8a775209e4d384ed8dedfd1eb13
dango-the-world <z.rohaso@gmail.com>
2025-01-08 05:07:48 +0900

    wip: hooks作成

--- src/app/_hooks/useGetDescription.ts
+++ src/app/_hooks/useGetDescription.ts
@@ -8,6 +8,24 @@ const useGetDescription = (id: string | null) => {
   useEffect(() => {
     const fetchData = async () => {
       setLoading(true);
+      try {
+        const res = await fetch(`api/hotpepper/description?id=${id}`);
+
+        if (!res.ok) {
+          throw new Error("データの取得に失敗しました。");
+        }
+
+        const json = await res.json();
+        setData(json);
+      } catch (err: unknown) {
+        console.error(err);
+      } finally {
+        setLoading(false);
+      }
     };
-  }, []);
+    fetchData();
+  }, [id]);
+  return { data, loading };
 };
+
+export default useGetDescription;
